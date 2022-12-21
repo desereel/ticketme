7768355103e3a68ad24d322a7e6bffcbea514809
Deseree Frink-Larcart <dfrinkla@nd.edu>
2022-12-21 14:43:05 -0600

    Logout Functionality

--- ticketme/ticketme/settings.py
+++ ticketme/ticketme/settings.py
@@ -128,4 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index" 
